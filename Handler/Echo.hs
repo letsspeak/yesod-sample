5139d6def625d2a0256d872086d542b70b63afcb
letsspeak <letsinfinite@gmail.com>
2013-05-29 17:57:26 +0000

    use templete

--- Handler/Echo.hs
+++ Handler/Echo.hs
@@ -3,6 +3,4 @@ module Handler.Echo where
 import Import
 
 getEchoR :: Text -> Handler RepHtml
-getEchoR theText = do
-    defaultLayout $ do
-        [whamlet|<h1>#{theText}|]
+getEchoR theText = defaultLayout $(widgetFile "echo")
